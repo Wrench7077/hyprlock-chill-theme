30f4b20eb59227564482e9eb7237883bb059294d
wrench77 <wrench7077@gmail.com>
2026-08-18 20:02:39 +0330

    Update playerinfo.sh

--- playerinfo.sh
+++ playerinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-complete_version=$(playerctl metadata --format '{{playerName}}  {{lc(status)}} :   {{trunc(title, 32)}}    󰠃{{artist}} {{duration(position)}}|{{duration(mpris:length)}}')
+player-stat=$(playerctl metadata --format '{{playerName}}  {{lc(status)}} :   {{trunc(title, 32)}}    󰠃{{artist}} {{duration(position)}}|{{duration(mpris:length)}}')
 
-echo "${complete_version}" 
+echo "${player-stat}" 
